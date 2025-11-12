b95a4cc79b8d1f352c98c95dc9479ea0cbdb0406
dekubruu <aktemirov2001@gmail.com>
2025-11-12 15:41:17 +0100

    [FIX] dd

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -28,7 +28,7 @@ class TestUsers(TransactionCase):
                 user = self.env['res.users'].create({
                     'name': 'Marie-Noël',
                     'login': 'mnv',
-                    'description'': None
+                    'description': None
                 })
                 
 
